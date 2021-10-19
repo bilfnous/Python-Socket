bb988a821b3cc5fc4305bdaac4fb9d0a29794c5a
B. Alfanous <b.alfanous@outlook.com>
2021-10-18 22:53:08 -0400

    programmed basic client that establishes a connection

--- py_socket/client.py
+++ py_socket/client.py
@@ -1,78 +1,15 @@
-import socket
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-s = socket.socket()
-host = input(str("Host name: "))
-port = 9000
+__file__ = "client.py"
+__auther__ = "B. Alfanous"
+__email__ = "b.alfanous@outlook.com"
+__date__ = "28/Oct/2019"
+
+import socket               # Import socket module
+
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 9000                # Reserve a port for your service.
 s.connect((host, port))
-print("Connected...")
-
-fileName = "demo.mp3"
-with open(fileName, 'rb') as file:
-	fileData = file.read(1024)
-	while fileData:
-		s.send(fileData)
-		fileData = file.read(1024)
-
-
-#file = open(fileName, 'rb')
-#fileData = file.read(1024)
-#s.send(fileData)
-
-import socket                   # Import socket module
-
-s = socket.socket()             # Create a socket object
-host = socket.gethostname()  #Ip address that the TCPServer  is there
-port = 50000                     # Reserve a port for your service every new transfer wants a new port or you must wait.
-
-s.connect((host, port))
-s.send("Hello server!")
-
-with open('demo1.mp3', 'wb') as f:
-    print ('file opened')
-    while True:
-        print('receiving data...')
-        data = s.recv(1024)
-        print('data=%s', (data))
-        if not data:
-            break
-        # write data to a file
-        f.write(data)
-
-f.close()
-print('Successfully get the file')
-s.close()
-print('connection closed')
-
-
-import socket                   # Import socket module
-
-port = 50000                    # Reserve a port for your service every new transfer wants a new port or you must wait.
-s = socket.socket()             # Create a socket object
-host = socket.gethostname()   # Get local machine name
-s.bind((host, port))            # Bind to the port
-s.listen(5)                     # Now wait for client connection.
-
-print ('Server listening....')
-
-
-while True:
-    conn, addr = s.accept()     # Establish connection with client.
-    print ('Got connection from', addr)
-    data = conn.recv(1024)
-    print('Server received', repr(data))
-
-    filename='demo.mp3' #In the same folder or path is this file running must the file you want to tranfser to be
-    f = open(filename,'rb')
-    l = f.read(1024)
-    while (l):
-       conn.send(l)
-       #print('Sent ',repr(l))
-       l = f.read(1024)
-    f.close()
-
-    print('Done sending')
-    conn.send('Thank you for connecting')
-    conn.close()
-
-
-############################################
+s.close()                     # Close the socket when done
