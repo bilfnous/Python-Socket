3946e1a31579dd4c89e0b44e2ffd162b1bd84c3c
B. Alfanous <b.alfanous@outlook.com>
2021-10-19 22:28:29 -0400

    server can send a file

--- py_socket/server.py
+++ py_socket/server.py
@@ -6,14 +6,42 @@ __auther__ = "B. Alfanous"
 __email__ = "b.alfanous@outlook.com"
 __date__ = "28/Mar/2020"
 
-import socket               # Import socket module
+import socket 
 
-s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 9000               # Reserve a port for your service.
-s.bind((host, port))        # Bind to the port
+# Reserve a port for your service.
+port = 9000
+# Get local machine name
+host = socket.gethostname()    
 
-s.listen(5)                 # Now wait for client connection.
-c, addr = s.accept()     # Establish connection with client.
-print ("Got connection from: ", addr)
-c.close()                # Close the connection
+# Create a socket object that uses IPv4 and TCP
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+print("Socket created.\n")       
+# Bind the socket to host and IP
+s.bind((host, port)) 
+print(f'Host: {host} Port: {port} are binded.\n')        
+# Now wait for client connection.
+s.listen(1)
+print("Server is listening...\n") 
+
+# Establish connection with client.                 
+clientconn, addr = s.accept()     
+print ("Connection established to: ", addr)
+
+print("\n**********************************************\n")  
+
+#fileName = input("Enter file name: ")
+#clientconn.send(bytes(fileName, "utf-8"))
+#print(f'Sending file: {fileName} \n')
+
+f = open('txt.txt', 'rb')
+
+for line in f:
+    clientconn.send(line)
+
+
+f.close()
+print("\n**********************************************\n")  
+
+# Close the connection
+clientconn.close()
+print("Socket Closed.\n")              
