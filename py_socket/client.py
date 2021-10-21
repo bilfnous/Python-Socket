ea015bef6e5868f4b7b86992d0fe6062b569a7de
B. Alfanous <b.alfanous@outlook.com>
2021-10-20 21:45:38 -0400

    fix wording

--- py_socket/client.py
+++ py_socket/client.py
@@ -9,7 +9,7 @@ __date__ = "28/Oct/2019"
 import socket
 
 # Get Server Address      
-host = input("Enter server address, enter for current machine: ") or socket.gethostname()
+host = input("Enter server address, or current machine will be used: ") or socket.gethostname()
 host = int(host)
 # Reserve a port for your service.
 port = 9000  
